9dd01047f66c287c6a62aa34bd034485737ab73a
Anya Vostinar <vostinar@carleton.edu>
2026-01-28 16:58:31 -0600

    starter files

---
+++ createtable.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS pokemon; --Remove the table if it already exists--
+CREATE TABLE pokemon ( --Create the table--
+  --Define the columns--
+  number INTEGER,
+  --Your code here--
+);
